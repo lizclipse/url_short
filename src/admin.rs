2ca761dc26347adfd7ed9c0b323fa108e5059c86
Liz <gammon.jocular.0w@icloud.com>
2023-01-31 23:22:33 +0000

    basic admin management

---
+++ src/admin.rs
@@ -0,0 +1,211 @@
+use aws_sdk_dynamodb::model::AttributeValue;
+use cookie::Cookie;
+use lambda_http::{http::Method, RequestExt};
+use serde::{Deserialize, Serialize};
+
+use crate::{Handler, Output};
+
+const COOKIE_NAME: &str = "admin_secret";
+const KEY: &str = "key";
+const URL: &str = "url";
+
+impl<'a> Handler<'a> {
+    pub async fn admin(mut self) -> Output {
+        let request = match self.admin_request() {
+            Ok(request) => request,
+            Err(err) => return err,
+        };
+
+        let err = if let Some(AdminRequest::Login(req)) = &request {
+            self.handle_login(&req)
+        } else {
+            self.authenticate()
+        };
+
+        match err {
+            Some(err) => err,
+            None => match request {
+                Some(AdminRequest::Upsert(req)) => self.upsert(req).await,
+                Some(AdminRequest::Delete(req)) => self.delete(req).await,
+                _ => self.page_admin::<String>(None).await,
+            },
+        }
+    }
+
+    async fn upsert(self, req: UpsertRequest) -> Output {
+        let err = self
+            .client
+            .put_item()
+            .table_name(&self.config.table_name)
+            .item(KEY, AttributeValue::S(req.key))
+            .item(URL, AttributeValue::S(req.url))
+            .send()
+            .await
+            .map(|_| None)
+            .unwrap_or_else(|err| Some(err.to_string()));
+
+        self.page_admin(err).await
+    }
+
+    async fn delete(self, req: DeleteRequest) -> Output {
+        let err = self
+            .client
+            .delete_item()
+            .table_name(&self.config.table_name)
+            .key(KEY, AttributeValue::S(req.key))
+            .send()
+            .await
+            .map(|_| None)
+            .unwrap_or_else(|err| Some(err.to_string()));
+
+        self.page_admin(err).await
+    }
+
+    async fn page_admin<E>(self, err: Option<E>) -> Output
+    where
+        E: AsRef<str>,
+    {
+        let rows = self
+            .client
+            .scan()
+            .table_name(&self.config.table_name)
+            .send()
+            .await
+            .map(|resp| resp.items.unwrap_or_default())
+            .unwrap_or_default()
+            .into_iter()
+            .flat_map(|mut item| {
+                let key = item
+                    .remove(KEY)
+                    .and_then(|key| match key {
+                        AttributeValue::S(key) => Some(key),
+                        _ => None,
+                    })
+                    .unwrap_or_default();
+                let url = item
+                    .remove(URL)
+                    .and_then(|url| match url {
+                        AttributeValue::S(url) => Some(url),
+                        _ => None,
+                    })
+                    .unwrap_or_default();
+                if key.is_empty() || url.is_empty() {
+                    None
+                } else {
+                    Some((key, url))
+                }
+            })
+            .map(|(key, url)| {
+                format!(
+                    include_str!("./templates/admin_row.html"),
+                    key = key,
+                    url = url
+                )
+            })
+            .collect::<Vec<_>>()
+            .join("");
+
+        self.render(
+            200,
+            format!(
+                include_str!("./templates/admin.html"),
+                error = err.map(|e| self.render_error(e)).unwrap_or_default(),
+                rows = rows,
+                nav = "",
+            ),
+        )
+    }
+
+    fn admin_request(&self) -> Result<Option<AdminRequest>, Output> {
+        if self.event.method() != Method::POST {
+            Ok(None)
+        } else {
+            self.event
+                .payload()
+                .map_err(|err| self.render(400, self.render_error(err.to_string())))
+        }
+    }
+
+    fn handle_login(&mut self, req: &LoginRequest) -> Option<Output> {
+        if req.secret == self.config.admin_secret {
+            self.add_cookie(
+                Cookie::build(COOKIE_NAME, &req.secret)
+                    .path("/")
+                    .secure(true)
+                    .http_only(true)
+                    .finish(),
+            );
+            None
+        } else {
+            Some(self.page_login(true))
+        }
+    }
+
+    fn authenticate(&self) -> Option<Output> {
+        let secret = self
+            .event
+            .headers()
+            .get("Cookie")
+            .and_then(|cookies| cookies.to_str().ok())
+            .and_then(|cookies| {
+                Cookie::split_parse_encoded(cookies).find(|cookie| {
+                    if let Ok(cookie) = cookie {
+                        cookie.name() == COOKIE_NAME
+                    } else {
+                        false
+                    }
+                })
+            })
+            .and_then(|secret| secret.ok());
+        let secret = secret.as_ref().map(|secret| secret.value());
+
+        if secret == Some(&self.config.admin_secret) {
+            None
+        } else {
+            // Render error page on failed login.
+            Some(self.page_login(secret.is_some()))
+        }
+    }
+
+    fn page_login(&self, invalid_secret: bool) -> Output {
+        self.render(
+            401,
+            format!(
+                include_str!("./templates/login.html"),
+                error = if invalid_secret {
+                    self.render_error("Secret is incorrect.")
+                } else {
+                    "".to_owned()
+                }
+            ),
+        )
+    }
+
+    fn render_error(&self, err: impl AsRef<str>) -> String {
+        format!(include_str!("./templates/error.html"), error = err.as_ref())
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+enum AdminRequest {
+    Login(LoginRequest),
+    Upsert(UpsertRequest),
+    Delete(DeleteRequest),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct LoginRequest {
+    secret: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct UpsertRequest {
+    key: String,
+    url: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct DeleteRequest {
+    key: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,97 @@
+mod admin;
+
 use aws_sdk_dynamodb::{model::AttributeValue, Client};
+use cookie::Cookie;
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 
-async fn function_handler(
-    client: &Client,
-    config: &Config,
-    event: Request,
-) -> Result<Response<Body>, Error> {
-    let params = event.path_parameters();
-    let key_param = params.first(&config.key_param);
+pub type Output = Result<Response<Body>, Error>;
 
-    match key_param {
-        Some("") | None => redirect_to(&config.default_redirect),
-        Some(key) => client
+pub struct Handler<'a> {
+    client: &'a Client,
+    config: &'a Config,
+    event: Request,
+
+    cookies: Vec<String>,
+}
+
+impl<'a> Handler<'a> {
+    pub fn new<'b>(client: &'b Client, config: &'b Config, event: Request) -> Handler<'b> {
+        Handler {
+            client,
+            config,
+            event,
+
+            cookies: vec![],
+        }
+    }
+
+    async fn run(self) -> Output {
+        let params = self.event.path_parameters();
+        let key_param = params.first(&self.config.key_param);
+
+        match key_param {
+            Some("") | None => redirect_to(&self.config.default_redirect),
+            Some(key) => {
+                if key == self.config.admin_key {
+                    self.admin().await
+                } else {
+                    self.process_redirect(key).await
+                }
+            }
+        }
+    }
+
+    async fn process_redirect(self, key: &str) -> Output {
+        self.client
             .get_item()
-            .table_name(&config.table_name)
+            .table_name(&self.config.table_name)
             .key("key", AttributeValue::S(key.to_owned()))
             .projection_expression("url")
             .send()
             .await
             .map_err(Box::new)?
             .item()
-            .ok_or("No redirect was found")
+            .ok_or((404, "No redirect was found"))
             .and_then(|item| {
                 item.get("url")
-                    .ok_or("The URL for this redirect does not exist")
+                    .ok_or((500, "The URL for this redirect does not exist"))
             })
             .and_then(|url| {
                 url.as_s()
-                    .map_err(|_| "The URL for this redirect is invalid")
+                    .map_err(|_| (500, "The URL for this redirect is invalid"))
             })
             .map(|url| redirect_to(url))
-            .unwrap_or_else(|err| render_page(404, err)),
+            .unwrap_or_else(|(status, err)| self.render(status, err))
+    }
+
+    fn render(&self, status: u16, body: impl AsRef<str>) -> Output {
+        let mut resp = Response::builder()
+            .status(status)
+            .header("Content-Type", "text/html");
+
+        for cookie in self.cookies.iter() {
+            resp = resp.header("Set-Cookie", cookie);
+        }
+
+        let resp = resp
+            .body(
+                format!(
+                    include_str!("./templates/_layout.html"),
+                    title = "Url Shortener",
+                    body = body.as_ref()
+                )
+                .into(),
+            )
+            .map_err(Box::new)?;
+        Ok(resp)
+    }
+
+    fn add_cookie(&mut self, cookie: Cookie) {
+        self.cookies.push(cookie.encoded().to_string());
     }
 }
 
-fn redirect_to(url: impl AsRef<str>) -> Result<Response<Body>, Error> {
+fn redirect_to(url: impl AsRef<str>) -> Output {
     let resp = Response::builder()
         .status(301)
         .header("Location", url.as_ref())
@@ -43,37 +100,12 @@ fn redirect_to(url: impl AsRef<str>) -> Result<Response<Body>, Error> {
     Ok(resp)
 }
 
-fn render_page(status: u16, message: impl AsRef<str>) -> Result<Response<Body>, Error> {
-    // TODO: Move template to a file
-    let resp = Response::builder()
-        .status(status)
-        .header("Content-Type", "text/html")
-        .body(
-            format!(
-                "
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset=\"utf-8\">
-    <title>Url Shortener</title>
-</head>
-<body>
-    <p>{}</p>
-</body>
-</html>
-",
-                message.as_ref()
-            )
-            .into(),
-        )
-        .map_err(Box::new)?;
-    Ok(resp)
-}
-
-struct Config {
+pub struct Config {
     table_name: String,
     key_param: String,
     default_redirect: String,
+    admin_key: String,
+    admin_secret: String,
 }
 
 impl Config {
@@ -81,10 +113,14 @@ impl Config {
         let table_name = std::env::var("TABLE_NAME")?;
         let key_param = std::env::var("KEY_PARAM")?;
         let default_redirect = std::env::var("DEFAULT_REDIRECT")?;
+        let admin_key = std::env::var("ADMIN_KEY")?;
+        let admin_secret = std::env::var("ADMIN_SECRET")?;
         Ok(Self {
             table_name,
             key_param,
             default_redirect,
+            admin_key,
+            admin_secret,
         })
     }
 }
@@ -104,7 +140,12 @@ async fn main() -> Result<(), Error> {
     let config = Config::new()?;
 
     run(service_fn(|event| {
-        function_handler(&client, &config, event)
+        Handler::new(&client, &config, event).run()
     }))
-    .await
+    .await?;
+
+    // TODO: see if this is called consistently upon lambda shutdown
+    tracing::info!("Shutting down");
+
+    Ok(())
 }
